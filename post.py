d3595c3ed653ac5518ec6c01fa1699bd7fd29685
rexim <reximkut@gmail.com>
2025-07-16 02:12:07 +0700

    Make post.py accept host and port via command line

--- post.py
+++ post.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
+
 # Reference Client implementation for POSTing via the PasteBEAM Protocol
 #
-# This script doesn't chunk the TCP stream by newlines like Erlang so
-# it may not work correctly. It's presented to convey the idea of the
-# protocol.
+# This script doesn't chunk the TCP stream by newlines like Erlang
+# does so it may not work correctly. It's presented here to convey the
+# idea of the protocol.
+
 import hashlib;
 from random import randint, randbytes
 from base64 import b64encode
 import socket
 import time
 import sys
-
-# TODO: accept address and port via command line
 
 RECV_SIZE = 1024
 POW_LIMIT = 50_000_000
@@ -30,15 +30,29 @@ def check_response_prefix(client, prefix: bytes) -> bytes:
         exit(1)
     return response.removeprefix(prefix)
 
+def usage(program_name: str):
+    print(f"Usage: {program_name} <host> <port> <file-path>")
+
 if __name__ != '__main':
     args = sys.argv
     program_name = args.pop(0)
 
     if len(args) == 0:
-        print(f"Usage: {program_name} <file-path>")
-        print(f"ERROR: no file-path is provided")
+        usage(program_name)
+        print(f"ERROR: no <host> is provided")
         exit(1)
+    host = args.pop(0)
 
+    if len(args) == 0:
+        usage(program_name)
+        print(f"ERROR: no <port> is provided")
+        exit(1)
+    port = int(args.pop(0))
+
+    if len(args) == 0:
+        usage(program_name)
+        print(f"ERROR: no <file-path> is provided")
+        exit(1)
     file_path = args.pop(0)
 
     with open(file_path) as f:
@@ -47,13 +61,13 @@ if __name__ != '__main':
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    client.connect(('localhost', 6969))
+    client.connect((host, port))
     check_response(client, b"HI\r\n")
-    print("OK: looks like PasteBEAM server")
+    print(f"{host}:{port}: looks like PasteBEAM server")
 
     client.send(b'POST\r\n')
     check_response(client, b"OK\r\n")
-    print("OK: server accepts POST")
+    print(f"{host}:{port}: server accepts POST")
 
     for line in content:
         client.send((line+'\r\n').encode())
@@ -61,9 +75,9 @@ if __name__ != '__main':
 
     client.send(b'SUBMIT\r\n')
     challenge = check_response_prefix(client, b'CHALLENGE ').strip().decode('utf-8')
-    print(f"OK: server challenged us with suffix {challenge}")
+    print(f"{host}:{port}: server challenged us with suffix {challenge}")
 
-    print(f"OK: mining the solution with {POW_LEADING_ZEROS} leading zeros in sha256 with {POW_LIMIT} iterations max")
+    print(f"{host}:{port}: mining the solution with {POW_LEADING_ZEROS} leading zeros in sha256 with {POW_LIMIT} iterations max")
 
     counter = 0
     while counter < POW_LIMIT:
@@ -75,11 +89,11 @@ if __name__ != '__main':
         while c < len(h) and h[c] == '0':
             c += 1
         if c >= POW_LEADING_ZEROS:
-            print(f"OK: found solution with prefix {prefix!r} and sha256 = {h!r}")
+            print(f"{host}:{port}: found solution with prefix {prefix!r} and sha256 = {h!r}")
             client.send(b'ACCEPTED ' + prefix + b'\r\n')
             post_id = check_response_prefix(client, b'SENT ').strip().decode('utf-8')
 
-            print(f"OK: Post ID: {post_id}")
+            print(f"{host}:{port}: Post ID: {post_id}")
             break
 
         counter += 1
