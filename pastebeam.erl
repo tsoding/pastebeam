54f390408656701f73ecd4772afac06c29c3f1db
rexim <reximkut@gmail.com>
2025-07-16 05:21:12 +0700

    Limit the amount of connections per IP

--- pastebeam.erl
+++ pastebeam.erl
@@ -14,41 +14,54 @@
 -export([start/0, start/2]).
 
 -define(DEFAULT_PORT, 6969).
--define(DEFAULT_POSTS, "./posts/").
+-define(DEFAULT_POSTS_ROOT, "./posts/").
 -define(POST_ID_BYTE_SIZE, 32).
 -define(POST_BYTE_SIZE_LIMIT, 4*1024).
 -define(CHALLENGE_TIMEOUT, 60*1000).
 -define(CHALLENGE_BYTE_SIZE, 32).
 -define(CHALLENGE_LEADING_ZEROS, 5).
-
-%% TODO: Limit the amount of connections from a single IP.
+-define(MAX_LIMIT_PER_IP, 10).
 
 -spec start() -> pid().
 start() ->
-    start(?DEFAULT_PORT, ?DEFAULT_POSTS).
+    start(?DEFAULT_PORT, ?DEFAULT_POSTS_ROOT).
 
--spec start(Port, Posts) -> pid() when
+-spec start(Port, PostsRoot) -> pid() when
       Port :: inet:port_number(),
-      Posts :: file:name_all().
-start(Port, Posts) ->
-    %% ok = filelib:ensure_dir(Posts),
+      PostsRoot :: file:name_all().
+start(Port, PostsRoot) ->
+    ok = filelib:ensure_dir(PostsRoot),
     Options = [binary, {packet, line}, {active, false}, {reuseaddr, true}],
-    Server = spawn(fun () -> server(Posts) end),
+    Server = spawn(fun () -> server(PostsRoot, #{}, #{}) end),
     {ok, LSock} = gen_tcp:listen(Port, Options),
     %% TODO: the accepter should be created and monitored by the server
     _IgnoringAccepter = spawn(fun () -> accepter(LSock, Server) end),
     Server.
 
--spec server(Posts) -> no_return() when
-      Posts :: file:name_all().
-server(Posts) ->
+-spec server(PostsRoot, Connections, Limits) -> no_return() when
+      PostsRoot   :: file:name_all(),
+      Connections :: #{ Pid :: pid() => {Socket :: gen_tcp:socket(), Addr :: addr()} },
+      Limits      :: #{ IP :: term() => integer() }.
+server(PostsRoot, Connections, Limits) ->
     receive
         {connected, Sock} ->
             {ok, Addr} = inet:peername(Sock),
-            {Pid, _Ref} = spawn_monitor(fun () -> session(command, Sock, Addr, Posts) end),
-            put(Pid, {Sock, Addr});
+            {IP, _Port} = Addr,
+            Limit = maps:get(IP, Limits, 0) + 1,
+            if
+                Limit > ?MAX_LIMIT_PER_IP ->
+                    io:format("~p: ERROR: too many connections\n", [Addr]),
+                    gen_tcp:send(Sock, <<"TOO MANY CONNECTIONS\r\n">>),
+                    gen_tcp:close(Sock),
+                    server(PostsRoot, Connections, Limits);
+                true ->
+                    NewLimits = maps:put(IP, Limit, Limits),
+                    {Pid, _Ref} = spawn_monitor(fun () -> session(command, Sock, Addr, PostsRoot) end),
+                    NewConnections = maps:put(Pid, {Sock, Addr}, Connections),
+                    server(PostsRoot, NewConnections, NewLimits)
+            end;
         {'DOWN', _Ref, process, Pid, Reason} ->
-            case get(Pid) of
+            case maps:get(Pid, Connections, undefined) of
                 {Sock, Addr} ->
                     case Reason of
                         normal ->
@@ -58,14 +71,22 @@ server(Posts) ->
                             gen_tcp:send(Sock, <<"500\r\n">>)
                     end,
                     gen_tcp:close(Sock),
-                    erase(Pid);
+                    NewConnections = maps:remove(Pid, Connections),
+                    {IP, _Port} = Addr,
+                    Limit = case maps:get(IP, Limits, 0) of
+                                Value when Value > 0  -> Value - 1;
+                                Value -> Value
+                            end,
+                    NewLimits = maps:put(IP, Limit, Limits),
+                    server(PostsRoot, NewConnections, NewLimits);
                 undefined ->
-                    io:format("WARNING: process ~p went down, but it was not associated with any sockets. Weird...\n", [Pid])
+                    io:format("WARNING: process ~p went down, but it was not associated with any sockets. Weird...\n", [Pid]),
+                    server(PostsRoot, Connections, Limits)
             end;
         Message ->
-            io:format("WARNING: Unknown message ~p\n", [Message])
-    end,
-    server(Posts).
+            io:format("WARNING: Unknown message ~p\n", [Message]),
+            server(PostsRoot, Connections, Limits)
+    end.
 
 -type addr() :: {inet:ip_address(), inet:port_number()} |
                 inet:returned_non_ip_address().
@@ -76,12 +97,12 @@ server(Posts) ->
                          {post, binary()} |
                          {get, unicode:chardata()}.
 
--spec session(State, Sock, Addr, Posts) -> ok when
+-spec session(State, Sock, Addr, PostsRoot) -> ok when
       State :: session_state(),
       Sock  :: gen_tcp:socket(),
       Addr  :: addr(),
-      Posts :: file:name_all().
-session(command, Sock, Addr, Posts) ->
+      PostsRoot :: file:name_all().
+session(command, Sock, Addr, PostsRoot) ->
     gen_tcp:send(Sock, <<"HI\r\n">>),
     io:format("~p: connected\n", [Addr]),
     case gen_tcp:recv(Sock, 0) of
@@ -90,10 +111,10 @@ session(command, Sock, Addr, Posts) ->
         {ok, <<"POST\r\n">>} ->
             gen_tcp:send(Sock, <<"OK\r\n">>),
             io:format("~p: wants to make a post\n", [Addr]),
-            session({post, <<"">>}, Sock, Addr, Posts);
+            session({post, <<"">>}, Sock, Addr, PostsRoot);
         {ok, <<"GET ", Id/binary>>} ->
             io:format("~p: wants to get a post\n", [Addr]),
-            session({get, string:trim(Id)}, Sock, Addr, Posts);
+            session({get, string:trim(Id)}, Sock, Addr, PostsRoot);
         {ok, Command} ->
             io:format("~p: ERROR: invalid command: ~p\n", [Addr, Command]),
             gen_tcp:send(Sock, "INVALID COMMAND\r\n"),
@@ -101,11 +122,11 @@ session(command, Sock, Addr, Posts) ->
         {error, Reason} ->
             exit(Reason)
     end;
-session({post, Content}, Sock, Addr, Posts) ->
+session({post, Content}, Sock, Addr, PostsRoot) ->
     case gen_tcp:recv(Sock, 0) of
         {ok, <<"SUBMIT\r\n">>} ->
             io:format("~p: submitted the post of size ~p bytes\n", [Addr, byte_size(Content)]),
-            session({challenge, Content}, Sock, Addr, Posts);
+            session({challenge, Content}, Sock, Addr, PostsRoot);
         {ok, Line} ->
             %% Is line a valid UTF-8?
             case unicode:characters_to_list(Line, utf8) of
@@ -131,7 +152,7 @@ session({post, Content}, Sock, Addr, Posts) ->
                                 true ->
                                     %% All good, adding the line
                                     gen_tcp:send(Sock, <<"OK\r\n">>),
-                                    session({post, <<Content/binary, Line/binary>>}, Sock, Addr, Posts)
+                                    session({post, <<Content/binary, Line/binary>>}, Sock, Addr, PostsRoot)
                             end;
                         _ ->
                             io:format("~p: ERROR: bad line ending\n", [Addr]),
@@ -142,12 +163,12 @@ session({post, Content}, Sock, Addr, Posts) ->
         {error, Reason} ->
             exit(Reason)
     end;
-session({challenge, Content}, Sock, Addr, Posts) ->
+session({challenge, Content}, Sock, Addr, PostsRoot) ->
     Challenge = base64:encode(crypto:strong_rand_bytes(?CHALLENGE_BYTE_SIZE)),
     gen_tcp:send(Sock, io_lib:bformat(<<"CHALLENGE sha256 ~p ~ts\r\n">>, [?CHALLENGE_LEADING_ZEROS, Challenge])),
     io:format("~p: has been challenged with prefix ~ts\n", [Addr, Challenge]),
-    session({accepted, Content, Challenge}, Sock, Addr, Posts);
-session({accepted, Content, Challenge}, Sock, Addr, Posts) ->
+    session({accepted, Content, Challenge}, Sock, Addr, PostsRoot);
+session({accepted, Content, Challenge}, Sock, Addr, PostsRoot) ->
     case gen_tcp:recv(Sock, 0, ?CHALLENGE_TIMEOUT) of
         {ok, <<"ACCEPTED ", Prefix/binary>>} ->
             io:format("~p: accepted the challenge\n", [Addr]),
@@ -162,18 +183,12 @@ session({accepted, Content, Challenge}, Sock, Addr, Posts) ->
                     io:format("~p: completed the challenge with hash: ~ts\n", [Addr, Hash]),
                     Id = random_valid_post_id(),
                     io:format("~p: assigned post id: ~ts\n", [Addr, Id]),
-                    PostPath = io_lib:format("~ts/~ts", [Posts, Id]),
+                    PostPath = io_lib:format("~ts/~ts", [PostsRoot, Id]),
                     %% TODO: try to regenerate the Id several times until you find the one that is not taken
-                    case filelib:is_regular(PostPath) of
-                        false ->
-                            ok = file:write_file(PostPath, Content),
-                            gen_tcp:send(Sock, [<<"SENT ">>, Id, <<"\r\n">>]),
-                            ok;
-                        true ->
-                            %% Very unlikely to happen, but still
-                            gen_tcp:send(Sock, [<<"500\r\n">>]),
-                            ok
-                    end;
+                    false = filelib:is_regular(PostPath), %% Very unlikely to happen, but still
+                    ok    = file:write_file(PostPath, Content),
+                    gen_tcp:send(Sock, [<<"SENT ">>, Id, <<"\r\n">>]),
+                    ok;
                 true ->
                     io:format("~p: ERROR: failed the challenge with hash: ~ts\n", [Addr, Hash]),
                     gen_tcp:send(Sock, <<"CHALLENGED FAILED\r\n">>),
@@ -190,13 +205,13 @@ session({accepted, Content, Challenge}, Sock, Addr, Posts) ->
         {error, Reason} ->
             exit(Reason)
     end;
-session({get, Id}, Sock, Addr, Posts) ->
+session({get, Id}, Sock, Addr, PostsRoot) ->
     case is_valid_post_id(Id) of
         true ->
             %% PostPath is safe to log with ~ts since it's made out of
-            %% Posts which we trust and Id which is verified with
+            %% PostsRoot which we trust and Id which is verified with
             %% is_valid_post_id/1.
-            PostPath = io_lib:format("~ts/~ts", [Posts, Id]),
+            PostPath = io_lib:format("~ts/~ts", [PostsRoot, Id]),
             case file:read_file(PostPath) of
                 {ok, Binary} ->
                     io:format("~p: sending out post ~ts\n", [Addr, PostPath]),
@@ -247,8 +262,7 @@ accepter(LSock, Server) ->
     Server ! {connected, Sock},
     accepter(LSock, Server).
 
-%% TODO: delete the posts by requiring the user to provide the CHALLENGE and ACCEPTED strings.
-%% TODO: maybe post ids should be uuids?
-%% TODO: Protocol versioning. The server may announce the version it supports in the "HI\r\n".
+%% TODO: Delete the posts by requiring the user to provide the CHALLENGE and ACCEPTED strings.
+%% TODO: Maybe post ids should be uuids?
 %% TODO: Some sort of heartbeat mechanism while the client is doing POW challenge.
 %% TODO: Should we support TLS connections?
