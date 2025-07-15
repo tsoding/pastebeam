4ef255d24ecd9cf3dc54a22f35fcc9edc9c0add2
rexim <reximkut@gmail.com>
2025-07-15 21:16:10 +0700

    Customizable ./posts/ folder

--- pastebeam.erl
+++ pastebeam.erl
@@ -1,8 +1,8 @@
 -module(pastebeam).
--export([start/1, accepter/1, session/3]).
+-export([start/0, start/2, accepter/2, session/4]).
 
-%% TODO: customizable ?POSTS folder
--define(POSTS, "./posts/").
+-define(DEFAULT_PORT, 6969).
+-define(DEFAULT_POSTS, "./posts/").
 
 %% TODO: protocol versioning
 %% TODO: limit the size of the uploaded file
@@ -13,13 +13,18 @@
 -type addr() :: {inet:ip_address(), inet:port_number()} |
                 inet:returned_non_ip_address().
 
--spec start(Port) -> pid() when
-      Port :: inet:port_number().
-start(Port) ->
-    ok = filelib:ensure_dir(?POSTS),
+-spec start() -> pid().
+start() ->
+    start(?DEFAULT_PORT, ?DEFAULT_POSTS).
+
+-spec start(Port, Posts) -> pid() when
+      Port :: inet:port_number(),
+      Posts :: file:name_all().
+start(Port, Posts) ->
+    ok = filelib:ensure_dir(Posts),
     Options = [binary, {packet, line}, {active, false}, {reuseaddr, true}],
     {ok, LSock} = gen_tcp:listen(Port, Options),
-    spawn(?MODULE, accepter, [LSock]).
+    spawn(fun () -> accepter(LSock, Posts) end).
 
 -spec fail_session(Sock, Addr, Reason) -> ok when
       Addr :: addr,
@@ -30,23 +35,26 @@ fail_session(Sock, Addr, Reason) ->
     gen_tcp:close(Sock),
     ok.
 
--spec session(State, Sock, Addr) -> ok when
-      State :: command |
-               {challenge, binary()} |
-               {accepted, binary(), binary()} |
-               {post, binary()} |
-               {get, unicode:chardata()},
-      Sock :: gen_tcp:socket(),
-     Addr :: addr().
-session(command, Sock, Addr) ->
+-type session_state() :: command |
+                         {challenge, binary()} |
+                         {accepted, binary(), binary()} |
+                         {post, binary()} |
+                         {get, unicode:chardata()}.
+
+-spec session(State, Sock, Addr, Posts) -> ok when
+      State :: session_state(),
+      Sock  :: gen_tcp:socket(),
+      Addr  :: addr(),
+      Posts :: file:name_all().
+session(command, Sock, Addr, Posts) ->
     io:format("~w: connected\n", [Addr]),
     case gen_tcp:recv(Sock, 0) of
         {ok, <<"POST\r\n">>} ->
             io:format("~w: wants to make a post\n", [Addr]),
-            session({post, <<"">>}, Sock, Addr);
+            session({post, <<"">>}, Sock, Addr, Posts);
         {ok, <<"GET ", Id/binary>>} ->
             io:format("~w: wants to get a post by id ~w\n", [Addr, Id]),
-            session({get, string:trim(Id)}, Sock, Addr);
+            session({get, string:trim(Id)}, Sock, Addr, Posts);
         {ok, Command} ->
             io:format("~w: ERROR: invalid command: ~w\n", [Addr, Command]),
             gen_tcp:send(Sock, "INVALID COMMAND\r\n"),
@@ -55,22 +63,22 @@ session(command, Sock, Addr) ->
         {error, Reason} ->
             fail_session(Sock, Addr, Reason)
     end;
-session({post, Content}, Sock, Addr) ->
+session({post, Content}, Sock, Addr, Posts) ->
     case gen_tcp:recv(Sock, 0) of
         {ok, <<"SUBMIT\r\n">>} ->
             io:format("~w: submitted the post of the ~p bytes\n", [Addr, byte_size(Content)]),
-            session({challenge, Content}, Sock, Addr);
+            session({challenge, Content}, Sock, Addr, Posts);
         {ok, Line} ->
-            session({post, <<Content/binary, Line/binary>>}, Sock, Addr);
+            session({post, <<Content/binary, Line/binary>>}, Sock, Addr, Posts);
         {error, Reason} ->
             fail_session(Sock, Addr, Reason)
     end;
-session({challenge, Content}, Sock, Addr) ->
+session({challenge, Content}, Sock, Addr, Posts) ->
     Challenge = binary:encode_hex(crypto:strong_rand_bytes(32)),
     gen_tcp:send(Sock, [<<"CHALLENGE ">>, Challenge, <<"\r\n">>]),
     io:format("~w: has been challenged\n", [Addr]),
-    session({accepted, Content, Challenge}, Sock, Addr);
-session({accepted, Content, Challenge}, Sock, Addr) ->
+    session({accepted, Content, Challenge}, Sock, Addr, Posts);
+session({accepted, Content, Challenge}, Sock, Addr, Posts) ->
     case gen_tcp:recv(Sock, 0) of
         {ok, <<"ACCEPTED ", Prefix/binary>>} ->
             io:format("~w: accepted the challenge\n", [Addr]),
@@ -82,7 +90,8 @@ session({accepted, Content, Challenge}, Sock, Addr) ->
                 <<"00000", _/binary>> ->
                     Id = binary:encode_hex(crypto:strong_rand_bytes(32)),
                     io:format("~w: completed the challenge: Id: ~ts\n", [Addr, Id]),
-                    ok = file:write_file(<<?POSTS, Id/binary>>, Content),
+                    PostPath = io_lib:format("~ts/~ts", [Posts, Id]),
+                    ok = file:write_file(PostPath, Content),
                     gen_tcp:send(Sock, [<<"SENT ">>, Id, <<"\r\n">>]),
                     gen_tcp:close(Sock),
                     ok;
@@ -100,15 +109,16 @@ session({accepted, Content, Challenge}, Sock, Addr) ->
         {error, Reason} ->
             fail_session(Sock, Addr, Reason)
     end;
-session({get, Id}, Sock, Addr) ->
+session({get, Id}, Sock, Addr, _Posts) ->
     io:format("~w: TODO: GET ~w\n", [Addr, Id]),
     gen_tcp:close(Sock),
     ok.
 
--spec accepter(LSock) -> no_return() when
-      LSock :: gen_tcp:socket().
-accepter(LSock) ->
+-spec accepter(LSock, Posts) -> no_return() when
+      LSock :: gen_tcp:socket(),
+      Posts :: file:name_all().
+accepter(LSock, Posts) ->
     {ok, Sock} = gen_tcp:accept(LSock),
     {ok, Addr} = inet:peername(Sock),
-    spawn(?MODULE, session, [command, Sock, Addr]),
-    accepter(LSock).
+    spawn(fun () -> session(command, Sock, Addr, Posts) end),
+    accepter(LSock, Posts).
